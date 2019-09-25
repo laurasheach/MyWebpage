f8fb2d5d6194e569d49d84c3d948b46990f4d0cc
Laura Sheach <laura_bubble_git@yahoo.com>
2019-09-25 16:07:16 +0100

    Started adding colour & wording to homepage

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
+
     <header>LAURA SHEACH</header>
 
     <nav>
-      <router-link :to="{ name: 'home' }">Home</router-link>  ||
-      <router-link :to="{ name: 'cv'}">CV</router-link>  ||
-      <router-link :to="{ name: 'projects'}">Projects</router-link>
+      <button><router-link :to="{ name: 'home' }">Home</router-link></button>
+      <button><router-link :to="{ name: 'cv'}">CV</router-link></button>
+      <button><router-link :to="{ name: 'projects'}">Projects</router-link></button>
     </nav>
 
     <body>
       <router-view id="view"></router-view>
     </body>
+
+    <footer>&#x24B8 Laura A Sheach</footer>
 
   </div>
 </template>
@@ -25,12 +28,60 @@ export default {
 </script>
 
 <style>
+
+* {
+  margin: 0;
+  padding: 0;
+  text-decoration: none;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
+header {
+  font-size: 50px;
+  padding: 10px;
+  color: #D0BCD5;
+  background-color: #181C1C;
+}
+
+nav {
+  padding: 10px;
+  background-color: #181C1C;
+}
+
+button {
+  margin: 10px;
+  padding: 0px;
+  color: #181C1C;
+  background-color: #D0BCD5;
+  border-radius: 10px;
+  width: 10vw;
+  height: 2vw;
+  font-size: 20px;
+  font-weight: bolder;
+  text-align: center;
+}
+
+button::after {
+    color: #181C1C;
+}
+
+body {
+  color: #D0BCD5;
+  background-color: #181C1C;
+}
+
+footer {
+  color: #D0BCD5;
+  background-color: #181C1C;
+  bottom: 0;
+  position: fixed;
+  width: 100vw;
+}
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,11 @@
 <template lang="html">
 
-  <div class="">
-    <p>Hi, I'm Laura Sheach....</p>
+  <div id="homepage">
+    <p>Hello, I'm Laura Sheach and I have just completed the Professional Software Development with <a href="https://codeclan.com/">CodeClan</a> in Glasgow, Scotland.</p>
+    <br>
+    <p>I originally began my journey in Software Development at University, initially studying Computer Science with Maths. However after my first year, I changed my degree to pursue a different path in accountancy. Since then I have had a rewarding career as a Chartered Accountant and also HGV Driver.</p>
+    <br>
+    <p>Following a break from my previous career, I decided to take the plunge and return to my original plan, choosing to begin with the Professional Software Development course, offered by CodeClan. The best way I can describe the course is “Brutal but brilliant”! It has tested me at every point but pushed me to expand my knowledge of Software Development. I have thoroughly enjoyed the experience and it’s fuelled my desire to learn more and make the step into the Tech Industry.</p>
   </div>
 
 
@@ -14,7 +18,18 @@ export default {
 </script>
 
 <style lang="css" scoped>
-body {
+
+#homepage {
   background-color: black;
+  color: #D0BCD5;
 }
+
+a {
+  text-decoration: underline;
+}
+
+a:visited {
+  color: #D0BCD5;
+}
+
 </style>
