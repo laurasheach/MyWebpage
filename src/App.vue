85e7ec0ab2c8c0371b9abe5f47b85ac2ef3852e5
Laura Sheach <laura_bubble_git@yahoo.com>
2019-09-25 18:33:30 +0100

    Homepage info centred. Styling & contact information added

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,25 @@
     <header>LAURA SHEACH</header>
 
     <nav>
-      <button><router-link :to="{ name: 'home' }">Home</router-link></button>
-      <button><router-link :to="{ name: 'cv'}">CV</router-link></button>
-      <button><router-link :to="{ name: 'projects'}">Projects</router-link></button>
+      <button><router-link :to="{ name: 'home' }">Home <i class="fas fa-home"></i></router-link></button>
+      <button><router-link :to="{ name: 'cv'}">CV <i class="fas fa-file"></i></router-link></button>
+      <button><router-link :to="{ name: 'projects'}">Projects <i class="fas fa-briefcase"></i></router-link></button>
     </nav>
 
     <body>
       <router-view id="view"></router-view>
     </body>
 
-    <footer>&#x24B8 Laura A Sheach</footer>
+    <footer>
+      <div id="contact">
+        <a class="contact-links" href="https://www.linkedin.com/in/laura-sheach/"><i class="fab fa-linkedin"></i></a>
+        <button class="email-button">
+          <a class="email" href="mailto:laurasheach@yahoo.co.uk?subject=Email From Site">laurasheach@yahoo.co.uk <i class="fas fa-envelope"></i></a>
+        </button>
+        <a class="contact-links" href="https://github.com/laurasheach"><i class="fab fa-github"></i></a>
+      </div>
+      <p>&#x24B8 Laura A Sheach</p>
+    </footer>
 
   </div>
 </template>
@@ -40,7 +49,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-
 }
 header {
   font-size: 50px;
@@ -57,7 +65,6 @@ nav {
 button {
   margin: 10px;
   padding: 0px;
-  color: #181C1C;
   background-color: #D0BCD5;
   border-radius: 10px;
   width: 10vw;
@@ -67,8 +74,29 @@ button {
   text-align: center;
 }
 
-button::after {
+button.email-button {
+  margin: 10px;
+  padding: 0px;
+  background-color: #D0BCD5;
+  border-radius: 10px;
+  width: 18vw;
+  height: 2vw;
+  font-size: 20px;
+  font-weight: bolder;
+  text-align: center;
+}
+
+a {
     color: #181C1C;
+}
+
+a.contact-links {
+  color: #D0BCD5;
+  font-size: 40px;
+}
+
+a.email {
+  color:  #181C1C;
 }
 
 body {
@@ -82,6 +110,13 @@ footer {
   bottom: 0;
   position: fixed;
   width: 100vw;
+  padding: 10px;
+}
+
+#contact {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,13 @@
 <template lang="html">
 
   <div id="homepage">
-    <p>Hello, I'm Laura Sheach and I have just completed the Professional Software Development with <a href="https://codeclan.com/">CodeClan</a> in Glasgow, Scotland.</p>
-    <br>
-    <p>I originally began my journey in Software Development at University, initially studying Computer Science with Maths. However after my first year, I changed my degree to pursue a different path in accountancy. Since then I have had a rewarding career as a Chartered Accountant and also HGV Driver.</p>
-    <br>
-    <p>Following a break from my previous career, I decided to take the plunge and return to my original plan, choosing to begin with the Professional Software Development course, offered by CodeClan. The best way I can describe the course is “Brutal but brilliant”! It has tested me at every point but pushed me to expand my knowledge of Software Development. I have thoroughly enjoyed the experience and it’s fuelled my desire to learn more and make the step into the Tech Industry.</p>
+    <div class="intro">
+      <p>Hello, I'm Laura Sheach and I have just completed the Professional Software Development with <a href="https://codeclan.com/">CodeClan</a> in Glasgow, Scotland. I am a quick learner and highly motivated individual with a curious mind who enjoys learning new skills and concepts, whilst collaborating with others to build solutions to problems.</p>
+      <br>
+      <p>I originally began my journey in Software Development at University, initially studying Computer Science with Maths. However after my first year, I changed my degree to pursue a different path in accountancy. Since then I have had a rewarding career as a Chartered Accountant and also HGV Driver.</p>
+      <br>
+      <p>Following a break from my previous career, I decided to take the plunge and return to my original plan, choosing to begin with the Professional Software Development course, offered by CodeClan. The best way I can describe the course is “Brutal but brilliant”! It has tested me at every point but pushed me to expand my knowledge of Software Development. I have thoroughly enjoyed the experience and it’s fuelled my desire to learn more and make the step into the Tech Industry.</p>
+    </div>
   </div>
 
 
@@ -20,8 +22,25 @@ export default {
 <style lang="css" scoped>
 
 #homepage {
+  margin: 0;
+  padding: 0;
   background-color: black;
   color: #D0BCD5;
+  height: 100vh;
+  width: 100vw;
+}
+
+.intro {
+  height: 50vh;
+  margin-left: 25vw;
+  margin-right: 25vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: justify;
+  text-justify: inter-word;
+  /* border: 1px solid green; */
 }
 
 a {
