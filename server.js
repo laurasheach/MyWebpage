8cc8fa66a141fd146ad3f7a4b38a18310705ba94
Laura Sheach <laura_bubble_git@yahoo.com>
2019-09-26 20:36:41 +0100

    Server.js adjustment

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
+// var express = require('express');
+// var path = require('path');
+// var serveStatic = require('serve-static');
+//
+// app = express();
+// app.use(serveStatic(__dirname + "/dist"));
+//
+// // var port = process.env.PORT || 5000;
+// app.set( 'port', ( process.env.PORT || 5000 ));
+// app.listen(app.get ('port'), function () {
+//   console.log('Node server is running on port  '+ app.get ('port'));
+// });
 
+const express = require('express');
+const serveStatic = require("serve-static")
+const path = require('path');
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
-
-// var port = process.env.PORT || 5000;
-app.set( 'port', ( process.env.PORT || 5000 ));
-app.listen(app.get ('port'), function () {
-  console.log('Node server is running on port  '+ app.get ('port'));
-});
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 80;
+app.listen(port);
